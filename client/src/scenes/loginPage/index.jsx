7d40785f8c79fd4e24faf24e5eacfa8c895470ad
Noahb26 <noahbrown2663@gmail.com>
2023-03-01 12:14:59 -0800

    add login and registration forms

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,38 @@
+// IMPORT REQUIRED PACKAGES //
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./form";
+
+// LOGIN FORM // 
 const LoginPage = () => {
-    return (<div>loginpage</div>)
-}
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Blog Bytes
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Blog Bytes, the blog for you!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
+};
 
 export default LoginPage;
